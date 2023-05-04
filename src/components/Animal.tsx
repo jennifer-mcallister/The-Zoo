5b5dff424dd992cd1a17ac6bba77f1eb190face1
Jennifer McAllister <113523615+jennifer-mcallister@users.noreply.github.com>
2023-05-04 16:31:43 +0200

    (feat): error handling on broken img and styling

--- src/components/Animal.tsx
+++ src/components/Animal.tsx
@@ -1,24 +1,26 @@
+import { ReactEventHandler, SyntheticEvent } from "react";
 import { IAnimal } from "../models/IAnimal"
+import "./Animal.css";
 
 interface IAnimalProps extends IAnimal {
     showprofil?: boolean;
 };
 
 export const Animal = (props: IAnimalProps) => {
-    if (props.showprofil) {
-        return (
-            <>
-                <p>{props.name}</p>
-                <p>{props.longDescription}</p>
-            </>
-        )
-    } else {
-        return (
-            <div>
-               <p>{props.name}</p> 
-            </div>
-        )
+    const unkownAnimal = "src/assets/unkown-animal.jpg"
+
+    const handleError = (e: SyntheticEvent<HTMLImageElement>) => {
+        e.currentTarget.src = unkownAnimal;
     }
+
+    return (
+        <div className="animal-container">
+            <h2>{props.name}</h2>
+            <img src={props.imageUrl} onError={handleError} />
+            <p>{props.shortDescription}</p>
+            
+        </div>
+    )
 
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { useLoaderData } from "react-router"
 import { Animal } from "../components/Animal";
 import { IAnimal } from "../models/IAnimal";
 import { Link } from "react-router-dom";
+import "./Home.css";
 
 
 export const Home = () => {
@@ -9,13 +10,16 @@ export const Home = () => {
     const animals= useLoaderData() as IAnimal[];
 
     return (
-        <>
-            <h1>Homepage</h1>
-            {animals.map((animal) => 
-            <Link key={animal.id} to={`${animal.id}`}>
-                 <Animal  {...animal} showprofil={false}></Animal>
-            </Link>
-            )}
-        </>
+        <div className="home-content">
+            <h1>My Zoo</h1>
+            <div className="animals-container">
+                {animals.map((animal) => 
+                <Link key={animal.id} to={`${animal.id}`}>
+                    <Animal  {...animal} showprofil={false}></Animal>
+                </Link>
+                )}
+            </div>
+           
+        </div>
     )
 }
